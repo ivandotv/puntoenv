41f7c8246867ba4d862b7fa820d59e310bf45a7c
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2023-02-16 21:01:18 +0100

    refactor tests

--- src/__tests__/filesystem/test-env/test-env.test.ts
+++ src/__tests__/filesystem/test-env/test-env.test.ts
@@ -1,14 +1,15 @@
 import { setupEnv } from 'index'
-import path from 'node:path'
 
 describe('env: TEST', () => {
   test('do not load .local files', () => {
     process.env.NODE_ENV = 'TEST'
-    const filePath = path.resolve(__dirname, './')
-    setupEnv(`${filePath}/`)
+    process.env.MY_ENV = 'original'
+
+    setupEnv(__dirname)
 
     expect(process.env.MY_ENV_TEST_LOCAL).toBeUndefined()
     expect(process.env.MY_ENV_LOCAL).toBeUndefined()
     expect(process.env.FROM_EXPAND).toEqual('my_expanded_value')
+    expect(process.env.MY_ENV).toEqual('original')
   })
 })
