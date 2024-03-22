05ee7c1172920ac5311b6c44d7b8bb13f986224e
Ivan Vlatković <390700+ivandotv@users.noreply.github.com>
2024-03-22 16:37:18 +0100

    Update vitest.config.ts with clearMocks option set to true

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   test: {
-    setupFiles: './vitestSetup.ts'
-  }
+    setupFiles: "./vitestSetup.ts",
+    clearMocks: true,
+  },
 })
